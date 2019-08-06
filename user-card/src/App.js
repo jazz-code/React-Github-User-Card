ed1190e63acfc77b71ba0cf16800f70d1e5773ba
jazz-code <ren.meta@gmail.com>
2019-08-06 18:41:41 -0400

    UserData component now displays login name

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import UserData from "./components/UserData.js";
 import axios from "axios";
 
 // import './App.css';
@@ -7,7 +8,8 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      users: []
+      data: []
+      // login: login
     };
   }
 
@@ -16,19 +18,24 @@ class App extends React.Component {
   }
 
   userData = () => {
+    // let data = avatar_url;
     axios
       .get(`https://api.github.com/users/jazz-code`)
       .then(res => res.data)
-      .then(users => this.setState({ user: users }));
+      .then(res =>
+        this.setState({
+          user: res,
+          login: res.login
+        })
+      );
   };
 
   render() {
-    console.log("users", this.state);
+    console.log("users", this.state.login);
     return (
       <div className="App">
-        <header className="App-header">
-          <p>{/* Edit <code>src/App.js</code> and save to reload. */}</p>
-        </header>
+        <header className="App-header" />
+        <UserData name={this.state.login} />
       </div>
     );
   }
